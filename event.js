060127d27125c9e99c027b7944dc0609ec531853
Devashish Nema <devnema3@gmail.com>
2024-07-31 18:56:06 +0530

    event ++ --

--- event.js
+++ event.js
@@ -1,14 +1,14 @@
-function button_click(){
-    alert("event working")
-}
+// function button_click(){
+//     alert("event working")
+// }
 
-function fun(){
-    alert("hey")
-}
+// function fun(){
+//     alert("hey")
+// }
 
-function fun2(){
-    alert("hello")
-}
+// function fun2(){
+//     alert("hello")
+// }
 
 // function num(){
 //     let num1 = document.getElementById("num1").value;
@@ -17,9 +17,40 @@ function fun2(){
 
 // }
 
-function num(){
-    let num1 = parseInt(document.getElementById("num1").value);
-    let num2 = parseInt(document.getElementById("num2").value);
-    document.write(num1+num2)
+// function num(){
+//     let num1 = parseInt(document.getElementById("num1").value);
+//     let num2 = parseInt(document.getElementById("num2").value);
+//     document.write(num1+num2)
 
+// }
+
+
+
+// function mouse(){
+//     alert("fine")
+// }
+
+// function m(){
+//     alert("okay")
+// }
+let a = 0
+let c = document.getElementById("id")
+let d = 0
+let b = document.getElementById("d")
+function add(){
+    a=a+500
+    d++
+    c.innerHTML=a 
+    b.innerHTML=d
+}
+
+function sub(){
+    if(a>0){
+        a=a-500
+        d--
+        
+        
+    }
+    c.innerHTML=a
+    b.innerHTML=d
 }
